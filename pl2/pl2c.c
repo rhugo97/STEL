0183b039cfa136028d659bb411da5280720996ec
Hugo Rodrigues <34515264+rhugo97@users.noreply.github.com>
2019-04-02 01:14:25 +0100

    Add files via upload

---
+++ pl2/pl2c.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "lista_ligada.c"
+
+#define lambda 200
+#define dm 0.008
+
+double delta= 0.001;
+double maxRep = 0.025;
+
+int main(){
+	//initializations
+	lista* list;
+	int tipo_ev, channels = 0, numberSim = 0, freeChannels=0, packetLost=0, packetsComing=0;
+	int cont=0, delays=0;
+	double timeEvent=0, c, u;
+	int intervals=(maxRep/delta);
+	double average=0, sum=0, timeDelay;
+	lista *queue;
+	int delayed=0, queueSize=0, queueSpaces=0, secondDelay=0;
+	double delayedMore=0;	
+	
+	int histogram[intervals];
+	
+	for(int i=0; i<intervals; i++){
+		histogram[i]=0;
+	}
+	// random number generator
+	time_t t;
+	srand((unsigned)time(&t));
+	
+	//Initialize the list
+	list=NULL;
+	list=adicionar(list,0,0);
+	
+	//list to the queue
+	queue=NULL;
+		
+
+	//number of samples
+	
+	printf("Number of Simulations: ");
+	scanf("%d", &numberSim);
+
+	printf("Number of channels: ");
+	scanf("%d", &channels);
+	freeChannels=channels;
+
+	printf("Size of queue: ");
+	scanf("%d", &queueSize);
+	queueSpaces=queueSize;
+
+	printf("Delayed more than: ");
+	scanf("%lf", &delayedMore);
+	
+	
+
+	//start of simulations
+	for(int i=0; i < numberSim; i++){
+		int tmp=0;
+		tipo_ev=list->tipo;
+		timeEvent=list->tempo;
+		list=remover(list);
+		
+		if(tipo_ev==0){
+			int tmp=0;
+			u= (double)((unsigned)rand())/((unsigned)RAND_MAX+1);
+			c = -log(u) / lambda;
+			packetsComing++;
+			
+			if(freeChannels>0){
+				freeChannels--;	
+				u= (double)((unsigned)rand())/((unsigned)RAND_MAX+1);
+
+				double d=-dm*log(u);
+				list=adicionar(list,1,timeEvent+d);
+				sum+=d;
+				
+			}
+			else{
+				if(queueSpaces>0){
+					queue=adicionar(queue,tipo_ev,timeEvent);
+					delays++;
+					queueSpaces--;
+				}
+				else{
+					packetLost++;
+				}
+				
+
+			}
+			list=adicionar(list,0,timeEvent+c);
+		}
+		else{
+			if(queueSpaces<queueSize){
+				timeDelay +=timeEvent-(queue->tempo);
+				if(delayedMore<(timeEvent-(queue->tempo))){
+					secondDelay++;
+				}
+				queue=remover(queue);
+				u= (double)((unsigned)rand())/((unsigned)RAND_MAX+1);
+				double d=-dm*log(u);
+				list=adicionar(list,1,timeEvent+d);
+				sum+=d;
+				//histogram
+				if(d>maxRep){
+					tmp = intervals-1;
+				}
+				else{
+					tmp = (int) (d/delta);
+				}
+
+		
+				histogram[tmp]++;
+				queueSpaces++;
+			}
+			else{
+				freeChannels++;
+			}
+		}
+		
+		
+
+
+		
+		
+	
+	}
+	for(int i=0; i<intervals; i++){
+		printf("Value of %d: %d\n", i+1, histogram[i]);
+	}
+	printf("Packet Blocking: %lf\n", ((double)packetLost/packetsComing)*100);
+	printf("Probability of delay: %lf\n", ((double)delays/packetsComing)*100);
+	printf("Probability of being delayed more than %lf : %lf \n", delayedMore, ((double)secondDelay/packetsComing)*100);
+	//imprimir(list);
+	//send to excel
+	FILE *fp;
+	fp=fopen("histdata.xsl","w");
+	for(int i=0; i<intervals; i++){
+		fprintf(fp,"%d,",histogram[i]);
+	}
+	fprintf(fp,"\n");
+	for(int i=0;i<intervals; i++){
+		fprintf(fp,"%d,",i);
+	}
+	
+	fclose(fp);
+
+	
+
+}
